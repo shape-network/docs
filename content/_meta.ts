adead4780019e6ba50fb03ae294579ee2577ab39
William H <william.hermozo@protonmail.com>
2025-04-03 16:24:27 +0200

    use ts meta files

---
+++ content/_meta.ts
@@ -0,0 +1,13 @@
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  index: 'Introducing Shape',
+  'getting-started': 'Getting Started',
+  'the-stack': 'The Stack',
+  'technical-details': 'Technical Details',
+  'building-on-shape': 'Building on Shape',
+  tools: 'Tools',
+  tutorials: 'Tutorials',
+};
+
+export default meta;
---
+++ content/tutorials/_meta.ts
@@ -0,0 +1,7 @@
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  'registering-contract-gasback': 'Registering Your Contract for Gasback',
+};
+
+export default meta;
