233228e3ca2a6cc6f57debac3b29660e12894d9c
William H <william.hermozo@protonmail.com>
2025-04-08 11:46:07 +0200

    imports in content

--- mdx-components.ts
+++ mdx-components.ts
@@ -1,15 +1,4 @@
 import { useMDXComponents as getThemeComponents } from 'nextra-theme-docs';
-import {
-  StackCallout,
-  GasbackCallout,
-  AddToStackCallout,
-  NetworkInformationCallout,
-  OtomsCallout,
-  TransactionFeesCallout,
-  RegisterGasbackCallout,
-  AlchemyApiCallout,
-} from '@components/callouts';
-import { ProjectCards, SuperchainIncentivesCards } from '@components/cards';
 
 const themeComponents = getThemeComponents();
 
@@ -17,15 +6,5 @@ export function useMDXComponents(components) {
   return {
     ...themeComponents,
     ...components,
-    GasbackCallout,
-    StackCallout,
-    AddToStackCallout,
-    NetworkInformationCallout,
-    OtomsCallout,
-    TransactionFeesCallout,
-    RegisterGasbackCallout,
-    ProjectCards,
-    AlchemyApiCallout,
-    SuperchainIncentivesCards,
   };
 }
