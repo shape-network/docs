d0260d97e393c419547d166b8f99befcda655512
William H <william.hermozo@protonmail.com>
2025-04-08 14:44:30 +0200

    card ui

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -22,7 +22,7 @@ export const Card: FC<{
   subtitle?: string;
 }> = ({ title, href, subtitle }) => {
   return (
-    <div className="rounded border border-neutral-200 bg-neutral-100 transition-colors hover:bg-neutral-100/25 dark:border-neutral-800 dark:bg-neutral-900 hover:dark:bg-neutral-900/50">
+    <div className="rounded border border-neutral-200 bg-neutral-50 transition-colors hover:bg-neutral-100/25 dark:border-neutral-800 dark:bg-neutral-900 hover:dark:bg-neutral-900/50">
       <Link href={href}>
         <span className="font-semibold">{title}</span>
         {subtitle && <span className="text-sm text-gray-500 dark:text-gray-400">{subtitle}</span>}
