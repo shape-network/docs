5c9701df0064901f1daf9664283b411ade4207e2
William H <william.hermozo@protonmail.com>
2025-04-08 19:42:58 +0200

    nu line

--- app/providers.tsx
+++ app/providers.tsx
@@ -14,6 +14,7 @@ if (typeof window !== 'undefined') {
     },
   });
 }
+
 export const CSPostHogProvider = ({ children }: PropsWithChildren) => {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 };
