921853175a3d2aff06db49e77a8dbe09d8f2a99d
William H <william.hermozo@protonmail.com>
2025-06-26 17:36:55 +0200

    make gasback first

--- content/building-on-shape/_meta.ts
+++ content/building-on-shape/_meta.ts
@@ -1,9 +1,9 @@
 import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
+  gasback: 'Gasback',
   'onchain-compatible': 'Onchain Compatible',
   'transaction-fees': 'Transaction Fees',
-  gasback: 'Gasback',
   'differences-ethereum-shape': 'Differences between Ethereum and Shape',
   'alchemy-api': 'Alchemy API',
   'superchain-incentives': 'Superchain Incentives',
