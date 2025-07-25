669d5521a3c963f9941062ad7c1d9b06e2e785db
William H <william.hermozo@protonmail.com>
2025-07-25 18:08:55 +0200

    add builder kit

--- content/building-on-shape/_meta.ts
+++ content/building-on-shape/_meta.ts
@@ -1,6 +1,7 @@
 import type { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
+  'builder-kit': 'Builder Kit',
   'onchain-compatible': 'Onchain Compatible',
   'transaction-fees': 'Transaction Fees',
   'differences-ethereum-shape': 'Differences between Ethereum and Shape',
